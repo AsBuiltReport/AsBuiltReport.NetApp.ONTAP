6627708c8012eb7e96bd5a50ecbe6e4f40a1e80e
Jonathan Colon <jcolonf@zenprsolutions.com>
2021-07-02 23:22:27 -0400

    Added ONTAP failed disk information

---
+++ Src/Private/Get-AbrOntapDiskBroken.ps1
@@ -0,0 +1,53 @@
+function Get-AbrOntapDiskBroken {
+    <#
+    .SYNOPSIS
+    Used by As Built Report to retrieve NetApp ONTAP failed disk information from the Cluster Management Network
+    .DESCRIPTION
+
+    .NOTES
+        Version:        0.1.0
+        Author:         Jonathan Colon
+        Twitter:        @jcolonfzenpr
+        Github:         rebelinux
+    .EXAMPLE
+
+    .LINK
+
+    #>
+    [CmdletBinding()]
+    param (
+    )
+
+    begin {
+        Write-PscriboMessage "Collecting ONTAP failed disk per node information."
+    }
+
+    process {
+        $NodeDiskBroken = Get-NcDisk | Where-Object{ $_.DiskRaidInfo.ContainerType -eq "broken" }
+        if ($NodeDiskBroken) {
+                $DiskFailed = foreach ($DiskBroken in $NodeDiskBroken) {
+                    [PSCustomObject] @{
+                        'Disk Name' = $DiskBroken.Name
+                        'Shelf' = $DiskBroken.Shelf
+                        'Bay' = $DiskBroken.Bay
+                        'Pool' = $DiskBroken.Pool
+                        'Disk Paths' = $DiskBroken.DiskPaths
+                        }
+                    }
+                    if ($Healthcheck.Storage.DiskStatus) {
+                        $DiskFailed | Set-Style -Style Critical -Property 'Disk Name','Shelf','Bay','Pool','Disk Paths'
+                    }
+            $TableParams = @{
+                Name = "Failed Disk Summary - $($ClusterInfo.ClusterName)"
+                List = $false
+            }
+            if ($Report.ShowTableCaptions) {
+                $TableParams['Caption'] = "- $($TableParams.Name)"
+            }
+            $DiskFailed | Table @TableParams
+        }
+    }
+
+    end {}
+
+}
