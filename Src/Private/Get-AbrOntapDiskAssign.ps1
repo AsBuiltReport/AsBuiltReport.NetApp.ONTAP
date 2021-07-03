45bb95ce727f3ca803e832533786cb9092aa0cb9
Jonathan Colon <jcolonf@zenprsolutions.com>
2021-07-02 23:22:04 -0400

    Added ONTAP disk assign summary information

---
+++ Src/Private/Get-AbrOntapDiskAssign.ps1
@@ -0,0 +1,47 @@
+function Get-AbrOntapDiskAssign {
+    <#
+    .SYNOPSIS
+    Used by As Built Report to retrieve NetApp ONTAP disk assign summary information from the Cluster Management Network
+    .DESCRIPTION
+
+    .NOTES
+        Version:        0.1.0
+        Author:         Jonathan Colon
+        Twitter:        @jcolonfzenpr
+        Github:         rebelinux
+    .EXAMPLE
+
+    .LINK
+
+    #>
+    [CmdletBinding()]
+    param (
+    )
+
+    begin {
+        Write-PscriboMessage "Collecting ONTAP disk assignment per node information."
+    }
+
+    process {
+        $NodeDiskCount = get-ncdisk | ForEach-Object{ $_.DiskOwnershipInfo.HomeNodeName } | Group-Object
+        if ($NodeDiskCount) {
+            $DiskSummary = foreach ($Disks in $NodeDiskCount) {
+                [PSCustomObject] @{
+                    'Node' = $Disks.Name
+                    'Disk Count' = $Disks | Select-Object -ExpandProperty Count
+                    }
+            }
+            $TableParams = @{
+                Name = "Assigned Disk Summary - $($ClusterInfo.ClusterName)"
+                List = $false
+            }
+            if ($Report.ShowTableCaptions) {
+                $TableParams['Caption'] = "- $($TableParams.Name)"
+            }
+            $DiskSummary | Table @TableParams
+        }
+    }
+
+    end {}
+
+}
