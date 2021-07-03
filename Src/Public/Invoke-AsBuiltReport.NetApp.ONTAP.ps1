a24bc043bbc9b39daba8e22fe1c44353464b6c96
Jonathan Colon <jcolonf@zenprsolutions.com>
2021-07-02 23:24:47 -0400

    Added storage and license information to the main report file

--- Src/Public/Invoke-AsBuiltReport.NetApp.ONTAP.ps1
+++ Src/Public/Invoke-AsBuiltReport.NetApp.ONTAP.ps1
@@ -68,25 +68,87 @@ function Invoke-AsBuiltReport.NetApp.ONTAP {
             #region Node Section
             Write-PScriboMessage "Node InfoLevel set at $($InfoLevel.Node)."
             if ($InfoLevel.Node -gt 0) {
-            Section -Style Heading2 'Node Summary' {
-            Paragraph "The following section provides a summary of the Node on $($ClusterInfo.ClusterName)."
-            BlankLine
-                Section -Style Heading3 'Node Inventory' {
-                    Paragraph "The following section provides the node inventory on $($ClusterInfo.ClusterName)."
+                Section -Style Heading2 'Node Summary' {
+                    Paragraph "The following section provides a summary of the Node on $($ClusterInfo.ClusterName)."
                     BlankLine
-                    Get-AbrOntapNodes
-                    Section -Style Heading4 'Node Hardware Inventory' {
-                        Paragraph "The following section provides the node hardware inventory on $($ClusterInfo.ClusterName)."
+                    Section -Style Heading3 'Node Inventory' {
+                        Paragraph "The following section provides the node inventory on $($ClusterInfo.ClusterName)."
                         BlankLine
-                        Get-AbrOntapNodesHW
+                        Get-AbrOntapNodes
+                        Section -Style Heading4 'Node Hardware Inventory' {
+                            Paragraph "The following section provides the node hardware inventory on $($ClusterInfo.ClusterName)."
+                            BlankLine
+                            Get-AbrOntapNodesHW
+                        }
+                        Section -Style Heading4 'Node Service-Processor Inventory' {
+                            Paragraph "The following section provides the node service-processor information on $($ClusterInfo.ClusterName)."
+                            BlankLine
+                            Get-AbrOntapNodesSP
+                        }
                     }
-                    Section -Style Heading4 'Node Service-Processor Inventory' {
-                        Paragraph "The following section provides the node service-processor information on $($ClusterInfo.ClusterName)."
+                }
+            }#endregion Node Section
+            #region Storage Section
+            Write-PScriboMessage "Storage InfoLevel set at $($InfoLevel.Node)."
+            if ($InfoLevel.Storage -gt 0) {
+                Section -Style Heading2 'Storage Summary' {
+                    Paragraph "The following section provides a summary of the storage hardware on $($ClusterInfo.ClusterName)."
+                    BlankLine
+                    Section -Style Heading3 'Aggregate Inventory' {
+                        Paragraph "The following section provides the Aggregates on $($ClusterInfo.ClusterName)."
                         BlankLine
-                        Get-AbrOntapNodesSP
+                        Get-AbrOntapStorageAGGR
+                    }
+                    Section -Style Heading3 'Disk Summary' {
+                        Paragraph "The following section provides the disk summary information on controller $($ClusterInfo.ClusterName)."
+                        BlankLine
+                        Section -Style Heading4 'Assigned Disk Summary' {
+                            Paragraph "The following section provides the number of disks assigned to each controller on $($ClusterInfo.ClusterName)."
+                            BlankLine
+                            Get-AbrOntapDiskAssign
+                        }
+                        Section -Style Heading4 'Disk Container Type Summary' {
+                            Paragraph "The following section provides a summary of disk status on $($ClusterInfo.ClusterName)."
+                            BlankLine
+                            Get-AbrOntapDiskType
+                        }
+                        Section -Style Heading4 'Failed Disk Summary' {
+                            Paragraph "The following section show failed disks on cluster $($ClusterInfo.ClusterName)."
+                            BlankLine
+                            Get-AbrOntapDiskBroken
+                        }
+                        Section -Style Heading4 'Disk Inventory' {
+                            Paragraph "The following section provides the Disks installed on $($ClusterInfo.ClusterName)."
+                            BlankLine
+                            Get-AbrOntapDiskInv
+                        }
+                    }
+                    If ($Nodeshelf) {
+                        Section -Style Heading3 'Shelf Inventory' {
+                            Paragraph "The following section provides the available Shelf on $($ClusterInfo.ClusterName)."
+                            BlankLine
+                            Get-AbrOntapDiskShelf
+                        }
+                    }
+                }
+            }#endregion Storage Section
+            #region Storage Section
+            Write-PScriboMessage "License InfoLevel set at $($InfoLevel.License)."
+            if ($InfoLevel.License -gt 0) {
+                Section -Style Heading2 'Licenses Summary' {
+                    Paragraph "The following section provides a summary of the license usage on $($ClusterInfo.ClusterName)."
+                    BlankLine
+                    Section -Style Heading3 'License Usage Summary' {
+                        Paragraph "The following section provides the installed licenses on $($ClusterInfo.ClusterName)."
+                        BlankLine
+                        Get-AbrOntapClusterLicense
+                        Section -Style Heading4 'License Feature Summary' {
+                            Paragraph "The following section provides the License Feature Usage on $($ClusterInfo.ClusterName)."
+                            BlankLine
+                            Get-AbrOntapClusterLicenseUsage
+                        }
                     }
                 }
             }
-        }#endregion Node Section
+        }
     }
-}
