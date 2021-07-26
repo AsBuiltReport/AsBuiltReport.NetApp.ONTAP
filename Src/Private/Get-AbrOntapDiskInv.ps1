4dcae66e518952206edd595780aff69d76f6b920
Jonathan Colon <jcolonf@zenprsolutions.com>
2021-07-26 10:02:28 -0400

    Split Disk Inventory Table for better columns handling

--- Src/Private/Get-AbrOntapDiskInv.ps1
+++ Src/Private/Get-AbrOntapDiskInv.ps1
@@ -42,7 +42,6 @@ function Get-AbrOntapDiskInv {
                     'Bay' = $Disks.Bay
                     'Capacity' = "$([math]::Round(($Disks.Capacity) / "1$($Unit)", 2))$Unit"
                     'Model' = $Disks.Model
-                    'SerialNumber' = $DiskType.SerialNumber
                     'Type' = $DiskType.DiskType
                 }
             }
@@ -51,6 +50,38 @@ function Get-AbrOntapDiskInv {
             }
             $TableParams = @{
                 Name = "Disk Inventory - $($ClusterInfo.ClusterName)"
+                List = $false
+            }
+            if ($Report.ShowTableCaptions) {
+                $TableParams['Caption'] = "- $($TableParams.Name)"
+            }
+            $DiskInventory | Table @TableParams
+        }
+        $DiskInv = Get-NcDisk
+        $NodeDiskBroken = Get-NcDisk | Where-Object{ $_.DiskRaidInfo.ContainerType -eq "broken" }
+        if ($DiskInv) {
+            $DiskInventory = foreach ($Disks in $DiskInv) {
+                $DiskType = Get-NcDisk -Name $Disks.Name | ForEach-Object{ $_.DiskInventoryInfo }
+                $DiskFailed = $NodeDiskBroken | Where-Object { $_.'Name' -eq $Disks.Name }
+                if ($DiskFailed.Name -eq $Disks.Name ) {
+                    $Disk = " $($DiskFailed.Name)(*)"
+                    }
+                    else {
+                        $Disk =  $Disks.Name
+                    }
+                [PSCustomObject] @{
+                    'Disk Name' = $Disk
+                    'Shelf' = $Disks.Shelf
+                    'Bay' = $Disks.Bay
+                    'SerialNumber' = $DiskType.SerialNumber
+                    'Type' = $DiskType.DiskType
+                }
+            }
+            if ($Healthcheck.Storage.DiskStatus) {
+                $DiskInventory | Where-Object { $_.'Disk Name' -like '*(*)' } | Set-Style -Style Critical -Property 'Disk Name'
+            }
+            $TableParams = @{
+                Name = "Disk Serial Number Inventory - $($ClusterInfo.ClusterName)"
                 List = $false
             }
             if ($Report.ShowTableCaptions) {
