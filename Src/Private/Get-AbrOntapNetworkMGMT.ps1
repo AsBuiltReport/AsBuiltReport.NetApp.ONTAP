52d69b4965334db56a7f15e42f656f9487d31774
Jonathan Colon <jcolonf@zenprsolutions.com>
2021-08-22 14:17:39 -0400

    ColumnWidths fixes

--- Src/Private/Get-AbrOntapNetworkMGMT.ps1
+++ Src/Private/Get-AbrOntapNetworkMGMT.ps1
@@ -29,20 +29,21 @@ function Get-AbrOntapNetworkMgmt {
             foreach ($Item in $ClusterData) {
                 $inObj = [ordered] @{
                     'Cluster Interface' = $Item.InterfaceName
-                    'Status' = $Item.OpStatus
+                    'Status' = $Item.OpStatus.ToString().ToUpper()
                     'Data Protocols' = $Item.DataProtocols
-                    'Vserver' = $Item.Vserver
                     'Address' = $Item.Address
+                    'Vserver' = $Item.Vserver
                 }
                 $ClusterObj += [pscustomobject]$inobj
             }
             if ($Healthcheck.Network.Interface) {
-                $ClusterObj | Where-Object { $_.'Status' -notlike 'up' } | Set-Style -Style Warning -Property 'Status'
+                $ClusterObj | Where-Object { $_.'Status' -notlike 'UP' } | Set-Style -Style Warning -Property 'Status'
             }
 
             $TableParams = @{
                 Name = "Cluster Network Information - $($ClusterInfo.ClusterName)"
                 List = $false
+                ColumnWidths = 35, 8, 21, 18, 18
             }
             if ($Report.ShowTableCaptions) {
                 $TableParams['Caption'] = "- $($TableParams.Name)"
@@ -55,20 +56,21 @@ function Get-AbrOntapNetworkMgmt {
             foreach ($Item in $ClusterData) {
                 $inObj = [ordered] @{
                     'MGMT Interface' = $Item.InterfaceName
-                    'Status' = $Item.OpStatus
+                    'Status' = $Item.OpStatus.ToString().ToUpper()
                     'Data Protocols' = $Item.DataProtocols
-                    'Vserver' = $Item.Vserver
                     'Address' = $Item.Address
+                    'Vserver' = $Item.Vserver
                 }
                 $ClusterObj += [pscustomobject]$inobj
             }
             if ($Healthcheck.Network.Interface) {
-                $ClusterObj | Where-Object { $_.'Status' -notlike 'up' } | Set-Style -Style Warning -Property 'Status'
+                $ClusterObj | Where-Object { $_.'Status' -notlike 'UP' } | Set-Style -Style Warning -Property 'Status'
             }
 
             $TableParams = @{
                 Name = "Management Network Information - $($ClusterInfo.ClusterName)"
                 List = $false
+                ColumnWidths = 35, 8, 21, 18, 18
             }
             if ($Report.ShowTableCaptions) {
                 $TableParams['Caption'] = "- $($TableParams.Name)"
@@ -81,20 +83,21 @@ function Get-AbrOntapNetworkMgmt {
             foreach ($Item in $ClusterData) {
                 $inObj = [ordered] @{
                     'Intercluster Interface' = $Item.InterfaceName
-                    'Status' = $Item.OpStatus
+                    'Status' = $Item.OpStatus.ToString().ToUpper()
                     'Data Protocols' = $Item.DataProtocols
-                    'Vserver' = $Item.Vserver
                     'Address' = $Item.Address
+                    'Vserver' = $Item.Vserver
                 }
                 $ClusterObj += [pscustomobject]$inobj
             }
             if ($Healthcheck.Network.Interface) {
-                $ClusterObj | Where-Object { $_.'Status' -notlike 'up' } | Set-Style -Style Warning -Property 'Status'
+                $ClusterObj | Where-Object { $_.'Status' -notlike 'UP' } | Set-Style -Style Warning -Property 'Status'
             }
 
             $TableParams = @{
                 Name = "Intercluster Network Information - $($ClusterInfo.ClusterName)"
                 List = $false
+                ColumnWidths = 35, 8, 21, 18, 18
             }
             if ($Report.ShowTableCaptions) {
                 $TableParams['Caption'] = "- $($TableParams.Name)"
@@ -107,20 +110,21 @@ function Get-AbrOntapNetworkMgmt {
             foreach ($Item in $ClusterData) {
                 $inObj = [ordered] @{
                     'Data Interface' = $Item.InterfaceName
-                    'Status' = $Item.OpStatus
+                    'Status' = $Item.OpStatus.ToString().ToUpper()
                     'Data Protocols' = [string]$Item.DataProtocols
-                    'Vserver' = $Item.Vserver
                     'Address' = $Item.Address
+                    'Vserver' = $Item.Vserver
                 }
                 $ClusterObj += [pscustomobject]$inobj
             }
             if ($Healthcheck.Network.Interface) {
-                $ClusterObj | Where-Object { $_.'Status' -notlike 'up' } | Set-Style -Style Warning -Property 'Status'
+                $ClusterObj | Where-Object { $_.'Status' -notlike 'UP' } | Set-Style -Style Warning -Property 'Status'
             }
 
             $TableParams = @{
                 Name = "Data Network Information - $($ClusterInfo.ClusterName)"
                 List = $false
+                ColumnWidths = 35, 8, 21, 18, 18
             }
             if ($Report.ShowTableCaptions) {
                 $TableParams['Caption'] = "- $($TableParams.Name)"
