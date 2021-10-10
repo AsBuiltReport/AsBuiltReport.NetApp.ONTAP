2e33b4ff52738e720593dca78781ac9f40490c99
Jonathan Colon <jcolonf@zenprsolutions.com>
2021-10-09 21:00:00 -0400

    Added function to convert empty culumns to "-".

--- Src/Private/SharedUtilsFunctions.ps1
+++ Src/Private/SharedUtilsFunctions.ps1
@@ -51,3 +51,38 @@ function Get-UnixDate ($UnixDate) {
     #>
     [timezone]::CurrentTimeZone.ToLocalTime(([datetime]'1/1/1970').AddSeconds($UnixDate))
 } # end
+function ConvertTo-EmptyToFiller {
+    <#
+    .SYNOPSIS
+    Used by As Built Report to convert empty culumns to "-".
+    .DESCRIPTION
+
+    .NOTES
+        Version:        0.4.0
+        Author:         Jonathan Colon
+
+    .EXAMPLE
+
+    .LINK
+
+    #>
+    [CmdletBinding()]
+    [OutputType([String])]
+    Param
+        (
+        [Parameter (
+            Position = 0,
+            Mandatory)]
+            [AllowEmptyString()]
+            [string]
+            $TEXT
+        )
+
+    switch ($TEXT) {
+            "" {"-"; break}
+            $Null {"-"; break}
+            "True" {"Yes"; break}
+            "False" {"No"; break}
+            default {$TEXT}
+        }
+    } # end
