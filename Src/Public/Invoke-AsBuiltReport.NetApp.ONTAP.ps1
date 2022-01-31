aabff2eff32b6d75d26eca917fecd11a21ae6961
Jonathan Colon <jcolonf@zenprsolutions.com>
2022-01-31 12:43:13 -0400

    Bumped version to 0.6.3

--- Src/Public/Invoke-AsBuiltReport.NetApp.ONTAP.ps1
+++ Src/Public/Invoke-AsBuiltReport.NetApp.ONTAP.ps1
@@ -5,7 +5,7 @@ function Invoke-AsBuiltReport.NetApp.ONTAP {
     .DESCRIPTION
         Documents the configuration of NetApp ONTAP in Word/HTML/Text formats using PScribo.
     .NOTES
-        Version:        0.6.2
+        Version:        0.6.3
         Author:         Jonathan Colon Feliciano
         Twitter:        @jcolonfzenpr
         Github:         rebelinux
