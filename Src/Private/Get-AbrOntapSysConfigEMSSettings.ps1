049611a287d678fed07cfe723bc2b21fd6fe14a3
Jonathan Colon <jcolonf@zenprsolutions.com>
2022-11-06 19:35:36 -0400

    Closes Add Audit log destination #30 and Add Audit Logs Support #31

--- Src/Private/Get-AbrOntapSysConfigEMSSettings.ps1
+++ Src/Private/Get-AbrOntapSysConfigEMSSettings.ps1
@@ -73,6 +73,78 @@ function Get-AbrOntapSysConfigEMSSetting {
         catch {
             Write-PscriboMessage -IsWarning $_.Exception.Message
         }
+        try {
+            $Data =  Get-NcAudit -Controller $Array
+            if ($Data) {
+                Section -Style Heading4 "Audit Settings" {
+                    Paragraph "The following section provides information about Audit Setting from $($ClusterInfo.ClusterName)."
+                    BlankLine
+                    $OutObj = @()
+                    foreach ($Item in $Data) {
+                        try {
+                            $inObj = [ordered] @{
+                                'Enable HTTP Get request' = ConvertTo-TextYN $Item.HttpGet
+                                'Enable ONTAPI Get request' = ConvertTo-TextYN $Item.OntapiGet
+                                'Enable CLI Get request' = ConvertTo-TextYN $Item.CliGet
+                            }
+                            $OutObj += [pscustomobject]$inobj
+                        }
+                        catch {
+                            Write-PscriboMessage -IsWarning $_.Exception.Message
+                        }
+                    }
+
+                    $TableParams = @{
+                        Name = "Audit Settings - $($ClusterInfo.ClusterName)"
+                        List = $true
+                        ColumnWidths = 40, 60
+                    }
+                    if ($Report.ShowTableCaptions) {
+                        $TableParams['Caption'] = "- $($TableParams.Name)"
+                    }
+                    $OutObj | Table @TableParams
+                    try {
+                        $Data =  Get-NcClusterLogForward -Controller $Array
+                        if ($Data) {
+                            Section -Style Heading4 "Audit Log Destinations" {
+                                $OutObj = @()
+                                foreach ($Item in $Data) {
+                                    try {
+                                        $inObj = [ordered] @{
+                                            'Destination' = $Item.Destination
+                                            'Facility' = $Item.Facility
+                                            'Port' = $Item.Port
+                                            'Protocol' = $Item.Protocol
+                                            'Server Verification' = ConvertTo-TextYN $Item.VerifyServerSpecified
+                                        }
+                                        $OutObj += [pscustomobject]$inobj
+                                    }
+                                    catch {
+                                        Write-PscriboMessage -IsWarning $_.Exception.Message
+                                    }
+                                }
+
+                                $TableParams = @{
+                                    Name = "Audit Log Destinations - $($ClusterInfo.ClusterName)"
+                                    List = $false
+                                    ColumnWidths = 20, 20, 20, 20, 20
+                                }
+                                if ($Report.ShowTableCaptions) {
+                                    $TableParams['Caption'] = "- $($TableParams.Name)"
+                                }
+                                $OutObj | Table @TableParams
+                            }
+                        }
+                    }
+                    catch {
+                        Write-PscriboMessage -IsWarning $_.Exception.Message
+                    }
+                }
+            }
+        }
+        catch {
+            Write-PscriboMessage -IsWarning $_.Exception.Message
+        }
     }
 
     end {}
